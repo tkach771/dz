f60b4130a72ca3c49d449f937e5c663ef05fabb3
vlad <tkach771@gmail.com>
2019-09-23 18:57:58 +0300

    смена цыкла

---
+++ src/main/java/Task4.java
@@ -0,0 +1,16 @@
+public class Task4 {
+
+    public static void main(String[] args) {
+        String name = "vlad";
+        int i = 0;
+        do {
+            int j = 0;
+            do {
+                System.out.print(name + " ");
+                j++;
+            } while (j < 10);
+            System.out.println();
+            i++;
+        } while (i < 5);
+    }
+}
